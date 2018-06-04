22c487165bd79053ede912233e010614b6dbab0f
hereisjai <jk9693@gmail.com>
2018-06-04 23:01:58 +0530

    Add files via upload

--- server.js
+++ server.js
@@ -23,7 +23,7 @@ const app = express();
 app.use( '/graphiql', graphiqlExpress({
 
   endpointURL: '/graphql',
-  subscriptionsEndpoint: `ws://localhost:3000/subscriptions`
+  subscriptionsEndpoint: `ws://http://graphqlserver-graphqlserver.a3c1.starter-us-west-1.openshiftapps.com/subscriptions`
 }));
   
 app.use('/graphql', bodyParser.json(), graphqlExpress({
